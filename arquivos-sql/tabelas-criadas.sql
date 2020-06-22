19a534fceef9f36ac01fecbba6c9acdae422a6f6
Ádrian Rabelo Mendes <49656873+Adrian-Rabelo@users.noreply.github.com>
2020-06-22 16:04:15 -0300

    Atualização das tabelas
    Alguns atributos foram alterados, adicionados ou removidos da tabela MySQL

--- arquivos-sql/tabelas-criadas.sql
+++ arquivos-sql/tabelas-criadas.sql
@@ -1,67 +1,91 @@
 create database bdfarmacia;
 use bdfarmacia;
+
 CREATE TABLE Farmacia (
-	id_farmacia INT primary key not null,
+    id_farmacia INT PRIMARY KEY NOT NULL,
     nome_farmacia VARCHAR(45),
     endereco VARCHAR(45)
 );
 
 CREATE TABLE Funcionario (
-	id_funcionario INT primary key not null,
+    id_funcionario INT PRIMARY KEY NOT NULL,
     nome_funcionario VARCHAR(45),
     endereco_funcionario VARCHAR(45),
-    rg varchar(14) not null,
-    cpf char(11) unique not null,
-    data_de_nascimento date,
-    data_de_admissao date,
-    sexo enum('Masculino', 'Feminino'),
-    login varchar(45) unique not null,
-    senha varchar(45) unique not null,
-    funcao varchar(45),
-    fk_id_departamento int not null,
-    fk_id_farmacia int not null,
-    fk_id_tipo_func int not null,
-    foreign key (fk_id_departamento)
-		references Departamento(id_departamento)
-			on delete cascade
-            on update cascade,
-	foreign key (fk_id_farmacia)
-		references Farmacia(id_farmacia)
-			on delete cascade
-            on update cascade,
-	foreign key (fk_id_tipo_func)
-		references Tipo_De_Funcionario(id_tipo)
-			on delete cascade
-            on update cascade
-    );
-create table Produto (
-	id_produto int primary key not null,
-    nome_produto varchar(45) not null,
-    fornecedor varchar(45) not null,
-    lote varchar(45) not null,
-    data_de_fabricacao date not null,
-    data_de_validade date not null,
-    codigo_de_barras double not null unique,
-    quantidade int,
-    tipo varchar(45),
-    tarja varchar(45) not null,
-    preco decimal(5,2) not null,
-    status enum('livre', 'reservado'),
-    obs_status varchar(255),
-    fk_id_farmacia int not null,
-    foreign key (fk_id_farmacia)
-		references Farmacia(id_farmacia)
-        on delete cascade
-        on update cascade
+    rg VARCHAR(14) NOT NULL,
+    cpf CHAR(11) UNIQUE NOT NULL,
+    data_de_nascimento DATE,
+    data_de_admissao DATE,
+    sexo ENUM('Masculino', 'Feminino'),
+    login VARCHAR(45) UNIQUE NOT NULL,
+    senha VARCHAR(45) NOT NULL,
+    fk_id_departamento INT NOT NULL,
+    fk_id_farmacia INT NOT NULL,
+    fk_id_tipo_func INT NOT NULL,
+    FOREIGN KEY (fk_id_departamento)
+        REFERENCES Departamento (id_departamento)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (fk_id_farmacia)
+        REFERENCES Farmacia (id_farmacia)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (fk_id_tipo_func)
+        REFERENCES Tipo_Funcionario (id_tipo)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
-create table Departamento ( 
-	id_departamento int primary key not null,
-    nome_departamento varchar(45)
-    );
-CREATE TABLE Tipo_De_Funcionario (
-	id_tipo int primary key not null,
-	tipo_de_funcionario varchar(45)
-    );
-    drop table tipo_de_funcionario;
-    drop table funcionario;
+CREATE TABLE Produto (
+    id_produto INT PRIMARY KEY NOT NULL,
+    nome_produto VARCHAR(45) NOT NULL,
+    fornecedor VARCHAR(45) NOT NULL,
+    lote VARCHAR(45) NOT NULL,
+    data_de_fabricacao DATE NOT NULL,
+    data_de_validade DATE NOT NULL,
+    codigo_de_barras DOUBLE NOT NULL UNIQUE,
+    quantidade INT,
+    tarja VARCHAR(45) NOT NULL,
+    preco DECIMAL(5 , 2 ) NOT NULL,
+    status ENUM('livre', 'reservado'),
+    fk_id_tipo_produto INT NOT NULL REFERENCES Tipo_Produto (id_tipo_produto)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    fk_id_farmacia INT NOT NULL,
+    FOREIGN KEY (fk_id_farmacia)
+        REFERENCES Farmacia (id_farmacia)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Venda (
+    id_venda INT PRIMARY KEY NOT NULL,
+    preco_venda DECIMAL(5 , 2 ),
+    quantidade_produto INT,
+    data_venda DATE,
+    fk_id_farmacia INT NOT NULL REFERENCES Farmacia (id_farmacia)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    fk_id_funcionario INT NOT NULL REFERENCES Funcionario (id_funcionario)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    fk_id_produto INT NOT NULL REFERENCES Produto (id_produto)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Departamento (
+    id_departamento INT PRIMARY KEY NOT NULL,
+    nome_departamento VARCHAR(45)
+);
+CREATE TABLE Tipo_Funcionario (
+    id_tipo INT PRIMARY KEY NOT NULL,
+    tipo_funcionario VARCHAR(45)
+);
+
+CREATE TABLE Tipo_Produto (
+    id_tipo_produto INT PRIMARY KEY NOT NULL,
+    tipo_produto VARCHAR(45)
+);
+
+CREATE TABLE Entregas_Reservadas (
+    id_entregas INT PRIMARY KEY NOT NULL,
+    nome VARCHAR(45),
+    rg VARCHAR(14),
+    cpf CHAR(11),
+    endereco VARCHAR(45),
+    complemento VARCHAR(50),
+    fk_id_venda INT NOT NULL REFERENCES Venda (id_venda)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
     
