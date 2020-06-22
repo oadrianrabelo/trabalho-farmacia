8c25217095e38c9772f0af715a7488bfb5a95189
Ádrian Rabelo Mendes <49656873+Adrian-Rabelo@users.noreply.github.com>
2020-06-22 17:47:04 -0300

    Atualização das classes DAO
    Classes DAO foram criadas e suas conexões com o banco de dados foram feitas no construtor

--- ProjetoFarmacia/src/com/projetofarmacia/DAO/FarmaciaDAO.java
+++ ProjetoFarmacia/src/com/projetofarmacia/DAO/FarmaciaDAO.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.projetofarmacia.DAO;
 
-/**
- *
- * @author Ádrian Rabelo
- */
+import com.projetofarmacia.jdbc.ConnectionFactory;
+import java.sql.Connection;
+
 public class FarmaciaDAO {
+    private final Connection conecta;
     
+    public FarmaciaDAO() {
+        this.conecta = new ConnectionFactory().conecta();
+    }
 }
--- ProjetoFarmacia/src/com/projetofarmacia/DAO/ProdutoDAO.java
+++ ProjetoFarmacia/src/com/projetofarmacia/DAO/ProdutoDAO.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.projetofarmacia.DAO;
 
-/**
- *
- * @author Ádrian Rabelo
- */
+import com.projetofarmacia.jdbc.ConnectionFactory;
+import java.sql.Connection;
+
 public class ProdutoDAO {
+    private final Connection conecta;
     
+    public ProdutoDAO() {
+        this.conecta = new ConnectionFactory().conecta();
+    }
 }
